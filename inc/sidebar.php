d7710603bc0ff323401d0dabee49c100077e6122
Alex U <info@alexu.dev>
2023-01-24 01:53:21 -0500

    Add github link

--- inc/sidebar.php
+++ inc/sidebar.php
@@ -20,26 +20,26 @@
             <i class="me-2 fa-brands fa-linkedin"></i>
             <a class="text-decoration-none text-black k5_links" href="https://www.linkedin.com/in/aulogov">Linkedin</a>
         </div>
-        <!-- <div class="my-3">
+        <div class="my-3">
             <i class="me-2 fa-brands fa-github"></i>
             <a class="text-decoration-none text-black k5_links" href="https://github.com/kysja">GitHub</a>
-        </div> -->
-
-        
-        <div class="mt-5 text-center border border-dark-subtle rounded-3 bg-body-tertiary p-3">
-            <h6 class="fw-bold">Download My Resume</h6>
-            <div class="mt-3">
-                <a class="fw-bold text-decoration-none btn btn-outline-dark px-3 mx-2 my-2" href="/resources/Resume_AlexUlogov.docx">
-                    <i class="fa-regular fa-file-word pe-2"></i> 
-                    docx
-                </a>
-                <a class="fw-bold text-decoration-none btn btn-outline-dark px-4 mx-2 my-2" href="/resources/Resume_AlexUlogov.pdf">
-                    <i class="fa-regular fa-file-pdf pe-2"></i> 
-                    pdf
-                </a>
-            </div>
         </div>
 
-
     </div>
+        
+    <div class="mt-5 mx-auto d-inline-block text-center border border-dark-subtle rounded-3 bg-body-tertiary py-3 px-3">
+        <h6 class="fw-bold">Download My Resume</h6>
+        <div class="mt-3">
+            <a class="fw-bold text-decoration-none btn btn-outline-dark px-3 mx-2 my-2" href="/resources/Resume_AlexUlogov.docx">
+                <i class="fa-regular fa-file-word pe-2"></i> 
+                docx
+            </a>
+            <a class="fw-bold text-decoration-none btn btn-outline-dark px-4 mx-2 my-2" href="/resources/Resume_AlexUlogov.pdf">
+                <i class="fa-regular fa-file-pdf pe-2"></i> 
+                pdf
+            </a>
+        </div>
+    </div>
+
+
 </aside>
